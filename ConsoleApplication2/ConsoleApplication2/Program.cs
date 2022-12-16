96abc7e76c73e0b684302dad09d8a72bed2ebe79
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-16 13:12:12 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("./res.txt");
+                string line = sr.ReadLine();
+                string result = line;
+                int n = line.Length;
+                while (line != null)
+                {
+                    if (line.Length > n)
+                    {
+                        result = line;
+                        n = line.Length;
+                    }
+                    line = sr.ReadLine();
+                }
+                Console.WriteLine("Самая длинная строка:");
+                Console.WriteLine(result);
+                Console.WriteLine("Её длина равна " + n);
+                sr.Close();
+            }
+            catch { Console.WriteLine("Файл не найден!!!"); }
+            Console.ReadLine();
+        }
+    }
+    }
+
